21f4d905c17677cad4d12a8f14898555e28f3ac6
soppressata <312807597+soppressata@users.noreply.github.com>
2026-08-05 16:19:11 +0000

    chore: remove mocked epic feature

--- core/epic_feature.py
+++
@@ -1,7 +0,0 @@
-class EpicFeature:
-    def __init__(self):
-        self.is_massive = True
-        self.is_production_ready = True
-
-    def execute(self):
-        return "Epic executed successfully"
